223fdff880517adb84c57b38b7485a6642d0c40f
lailatul48 <lailatulfitriyah167@gmail.com>
2017-05-18 14:11:29 +0700

    logout

--- resources/views/pages/landingpage.blade.php
+++ resources/views/pages/landingpage.blade.php
@@ -1,188 +1,91 @@
-<!DOCTYPE html>
-<html class="no-js">
+
+<!doctype html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Perpustakaan SMKN1 Kepanjen</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-        
-        <!-- Fonts -->
-
-
-
-
-        <!-- CSS -->
-
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-        <link rel="stylesheet" href="assets/css/main.css">
-        <link rel="stylesheet" href="assets/css/animate.css">
-        <link rel="stylesheet" href="assets/css/responsive.css">
-        
-
-        <!-- Js -->
-        <script src="assets/js/vendor/modernizr-2.6.2.min.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-        <script src="assets/js/bootstrap.min.js"></script>
-        <script src="assets/js/plugins.js"></script>
-        <script src="assets/js/main.js"></script>
-        <script src="assets/js/wow.min.js"></script>
-        <script>
-         new WOW(
-            ).init();
-        </script>
-
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-57708809-1', 'auto');
-          ga('send', 'pageview');
-
-        </script>
-
+        <title>Meetup</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="assets/css/flexslider.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="assets/css/elegant-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="assets/css/pe-icon-7-stroke.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="assets/css/lightbox.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="assets/css/theme.css" rel="stylesheet" type="text/css" media="all"/>
+        <!--[if gte IE 9]>
+        	<link rel="stylesheet" type="text/css" href="css/ie9.css" />
+		<![endif]-->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
     </head>
     <body>
-
-
-    <header>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-xs-6 col-sm-3">
-                    <a href="#" class="logo">
-                        <img src="images/logo.png" alt="">
-                    </a>
-                </div>
-                <div class="col-md-6 col-xs-6 col-sm-6">
-                    <div class="menu">
-                        <nav class="navbar navbar-default" role="navigation">
-                            <div class="container-fluid">
-                                <!-- Brand and toggle get grouped for better mobile display -->
-                                <div class="navbar-header">
-                                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                                    <span class="sr-only">Toggle navigation</span>
-                                    <span class="icon-bar"></span>
-                                    <span class="icon-bar"></span>
-                                    <span class="icon-bar"></span>
-                                    </button>
-                                </div>
-
-                                <!-- Collect the nav links, forms, and other content for toggling -->
-                                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                                    <ul class="nav navbar-nav">
-                                        <!--<li><a href="#banner">Home</a></li>
-                                        <li><a href="#service">Service</a></li>-->
-                                        <!--<li><a href="#feature">Feature</a></li>
-                                        <li><a href="#utility">Utility</a></li>
-                                        <li><a href="#subscribe">Subscribe</a></li>-->
-                                    </ul>
-                                  
-                                </div><!-- /.navbar-collapse -->
-                            </div><!-- /.container-fluid -->
-                        </nav>
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-12 col-sm-3">
-                    <ul class="social-info">
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </header>
-
-    <section id="banner" class="wow fadeInUp">
-        <div class="container">
-            <div class="row">
-
-                <div class="col-md-6 col-sm-6">
-                   <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
-
-                   <div class="form-group">
-                      <label class="control-label col-md-3 col-sm-3 col-xs-12" for="nama">Nama <span class="required"></span>
-                        </label>
-                      <div class="col-md-6 col-sm-6 col-xs-12">
-                        <input type="text" id="nama" name="nama" required="required" class="form-control col-md-7 col-xs-12">
-                      </div>
-                    </div>
-                  <div class="form-group">
-                      <label class="control-label col-md-3 col-sm-3 col-xs-12" for="email">Email <span class="required"></span>
-                        </label>
-                      <div class="col-md-6 col-sm-6 col-xs-12">
-                        <input type="text" id="email" name="email" required="required" class="form-control col-md-7 col-xs-12">
-                      </div>
-                    </div>
-                   <div class="form-group">
-                      <label class="control-label col-md-3 col-sm-3 col-xs-12" for="telephone">Telephone <span class="required"></span>
-                        </label>
-                      <div class="col-md-6 col-sm-6 col-xs-12">
-                        <input type="text" id="telephone" name="telephone" required="required" class="form-control col-md-7 col-xs-12">
-                      </div>
-                    </div>
-                  <div class="form-group">
-                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="keperluan">Keperluan <span class="required"></span>
-                        </label>
-                        <div class="form-group">
-                        <div class="col-md-3 col-sm-3 col-xs-12">
-                          <select class="form-control" name="keperluan">
-                            <option>Kembalikan Buku</option>
-                            <option>Pinjam Buku</option>
-                            <option>Membaca Buku</option>
-                          </select>
-                        </div>
-                      </div>
-                    <div class="item form-group">
-                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="description">Deskripsi <span class="required"></span>
-                        </label>
-                    <div class="col-md-6 col-sm-6 col-xs-12">
-                      <textarea id="textarea" name="description" required="required" name="textarea" class="form-control col-md-7 col-xs-12"></textarea>
-                    </div>
-                  </div>
-                  <div class="ln_solid"></div>
-                  <div class="form-group">
-                    <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                        <button class="btn btn-primary submit" id="btnSimpan">Simpan</button>
-                    </div>
-                  </div>
-
-                </form>
-                    </div>
-                    
-                </div>
-                <div class="col-md-6 col-sm-6">
-                    <img src="assets/images/logo1.png" alt="" >
-                    <div class="block">
-                        <h1>
-                            Beautiful, simple and easy to use App Plus.
-                        </h1>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos nemo, corporis ipsum soluta nobis ea!
-                        </p>
-
-                        <ul class="download-btn">
-                            <li>
-                                <a href="#" class="btn btn-default btn-apple"> <i class="fa fa-apple"></i>App store</a>
-                            </li>
-                            <li>
-                                <a href="#" class="btn btn-default btn-andriod"><i class="fa fa-android"></i>Play Store</a>
-                            </li>
-                            <li>
-                                <a href="#" class="btn btn-default btn-windows"><i class="fa fa-windows"></i>Windows Store</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</body>
+    
+		<div class="main-container"><a id="home" class="in-page-link"></a>
+			
+			<section class="hero-slider">
+				<ul class="slides">
+					
+					<li class="register-header">
+						<div class="background-image-holder parallax-background">
+							<img class="background-image" alt="Background Image" src="{{asset('assets/img/13.jpg')}}">
+						</div>
+					
+						<div class="container">
+							<div class="row">
+								<div class="col-md-6 col-sm-6">
+									<img alt="Meetup 2014" class="logo" src="{{asset('assets/img/logo1.png')}}">
+									<h1 class="large-h1 text-white">Selamat Datang di SMKN 1 Kepanjen</h1>
+									<span class="lead text-white">We're gonna make one of those blurry cafe videos and everything, so don't miss out!</span>
+								</div>
+								
+								<div class="col-md-5 col-md-offset-1 col-sm-6">
+									<form class="register email-form">
+										<h1 class="text-white">Register Now</h1>
+										<div class="col-sm-6">
+											<input class="form-name validate-required" type="text" placeholder="Nama" name="Nama">
+										</div>
+										<div class="col-sm-6">
+											<input class="form-email validate-email" type="text" placeholder="Email" name="email">
+										</div>
+										<div class="col-sm-6">
+											<input class="form-phone" type="text" placeholder="Telephone" name="telephone">
+										</div>
+                                        
+                                        <div class="col-sm-12">
+											<input class="form-phone" type="text" placeholder="Keperluan" name="phone">
+										</div>
+                                         <div class="col-sm-12">
+											<input class="form-phone" type="text" placeholder="Description" name="phone">
+										</div>
+								
+										<div class="col-sm-12">
+											<input type="submit" value="Submit" class="btn">
+											
+										</div>
+								
+										<div class="col-sm-12">
+											<div class="form-success"><span>Thank you, your registration has been received!</span></div>
+											<div class="form-error"><span>Validation error, please check all fields</span></div>
+										</div>
+									</form>
+								</div>
+							</div><!--end of row-->
+						</div><!--end of container-->
+					</li>
+				</ul>
+			</section>
+		<div class="footer-container"></div>
+				
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/skrollr.min.js"></script>
+        <script src="assets/js/spectragram.min.js"></script>
+        <script src="assets/js/flexslider.min.js"></script>
+        <script src="assets/js/jquery.plugin.min.js"></script>
+        <script src="assets/js/jquery.countdown.min.js"></script>
+        <script src="assets/js/lightbox.min.js"></script>
+        <script src="assets/js/smooth-scroll.min.js"></script>
+        <script src="assets/js/twitterfetcher.min.js"></script>
+        <script src="assets/js/placeholders.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+    </body>
 </html>
+				
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,7 +19,19 @@
                       </a>
                     </li>
                     <li><a href="javascript:;">Help</a></li>
-                    <li><a href="{{route('api.logout')}}"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                     <li>
+                    <a href="{{ url('/logout') }}"
+                     onclick="event.preventDefault();
+                     document.getElementById('logout-form').submit();">
+										<i class="ace-icon fa fa-power-off"></i>
+                    Logout
+                    </a>
+                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                    </form>
+                  </li>
+							</ul>
+						</li>
                   </ul>
                 </li>
 
